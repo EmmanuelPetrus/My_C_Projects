606063e6c197767db6c342e20ffc73dd958f280c
Emmanuel Peter <peterojonilemi@gmail.com>
2022-06-01 15:19:12 +0100

    Fixed the bugs and now the program is fully working

--- Login_System_Pointers.c
+++ Login_System_Pointers.c
@@ -5,14 +5,10 @@
 
 int main(void)
 {
-    details person;
-    int length = 0;
-    details *person_store = &person;
-    int *len_store = &length;
+    int opt, usrFound = 0;
     FILE *fp;
-    int opt, usrFound;
-    char a = 'n', b = 'm', c = '2';
-    char *user_name;
+    person info, info_store;
+    person *point_info = &info;
     system("cls");
     system("color 0b");
     printf("\n\t\t\t------------Welcome to authentication system----------------");
@@ -28,83 +24,81 @@ int main(void)
     switch (opt)
     {
     case 1:
-        system("cls");
-        // char *password2;
-        char *name = take_input(person_store, a, len_store);
-        char *phone_no = take_input(person_store, c, len_store);
-        char *email = take_input(person_store, b, len_store);
-        char *pwd = take_password(person_store, c);
-        char *pwd2 = take_password(person_store, a);
-        if (!strcmp(pwd, pwd2))
-        {
+        take_input(point_info, 'n');
+        take_input(point_info, 'e');
+        take_input(point_info, 'p');
+        take_password(point_info, 1);
+        take_password(point_info, 2);
 
-            user_name = generate_username(person_store, len_store);
-            printf("\nYour username is %s", person_store->username);
-            printf("\nYour fullname is %s", person_store->fname);
-            fp = fopen("Users2.dat", "a+");
-            fwrite(person_store, sizeof(details), 1, fp);
-            if (fp == NULL)
+        if (!(compare_str(point_info->pword, point_info->pword2)))
+        {
+            generate_username(point_info);
+            printf("\nYour username is %s", point_info->username);
+            fp = fopen("Users2.txt", "a+");
+            if ((fp == NULL))
             {
-                fprintf(stderr, "Error opening file for writing");
-                exit(1);
+                fprintf(stderr, "%s", "Failed to open file ");
+                break;
             }
             else
-                printf("\n\nUser registration successful, Your username is %s", user_name);
-            fclose(fp);
-            free_memory(name, email, user_name, phone_no, pwd, pwd2);
+            {
+                if (!(fwrite(&info, sizeof(person), 1, fp)))
+                {
+                    fprintf(stderr, "%s", "Registration unsuccessfull");
+                    break;
+                }
+                else
+                {
+                    printf("Registration Successfull");
+                }
+            }
         }
-        else
-        {
-            printf("\nPassword do not match");
-        }
-        free_memory(name, email, user_name, phone_no, pwd, pwd2);
+        fclose(fp);
         break;
-        // case 2:
+    case 2:
+        take_input(point_info, 'u');
+        take_password(point_info, 1);
+        fp = fopen("Users2.txt", "r");
+        if (fp == NULL)
+        {
+            fprintf(stderr, "Error opening file for reading");
+            exit(1);
+        }
+        printf("\nYour password is %s\n\n", info.pword);
+        while (fread(&info_store, sizeof(person), 1, fp))
+        {
+            if (!compare_str(info_store.username, info.username))
+            {
 
-        //     printf("\nEnter your username:\t");
-        //     takeinput(username);
-        //     printf("Enter your password:\t");
-        //     takepassword(pword);
-        //     fp = fopen("Users2.txt", "r");
-        //     if (fp == NULL)
-        //     {
-        //         fprintf(stderr, "Error opening file for reading");
-        //         exit(1);
-        //     }
+                system("cls");
+                if (!compare_str(info.pword, info_store.pword))
+                {
+                    system("cls");
+                    printf("\n\t\t\t\t\t\t\t\t\tWelcome %s", info_store.fname);
+                    printf("\n\n|Full Name:\t%s", info_store.fname);
+                    printf("\n|Email:\t\t%s", info_store.email);
+                    printf("\n|Username:\t%s", info_store.username);
+                    printf("\n|Contact no.:\t%s", info_store.phone_num);
+                }
 
-        //     while (fread(&usr, sizeof(struct user), 1, fp))
-        //     {
-        //         if (!strcmp(usr.username, username))
-        // {
-        //     system("cls");
-        //     if (!strcmp(usr.password, pword))
-        //     {
-        //         system("cls");
-        //         printf("\n\t\t\t\t\t\t\t\t\tWelcome %s", usr.fullName);
-        //         printf("\n\n|Full Name:\t%s", usr.fullName);
-        //         printf("\n|Email:\t\t%s", usr.email);
-        //         printf("\n|Username:\t%s", usr.username);
-        //         printf("\n|Contact no.:\t%s", usr.phone);
-        //     }
-        //     else
-        //     {
-        //         printf("\n\nInvalid Password!");
-        //         Beep(800, 300);
-        //     }
-        //     usrFound = 1;
-        // }
-        //     }
-        //     if (!usrFound)
-        //     {
-        //         printf("\n\nUser is not registered!");
-        //         Beep(800, 300);
-        //     }
-        //     fclose(fp);
-        //     break;
-        // case 3:
-        //     printf("\t\t\t Bye Bye :)");
-        //     break;
+                else
+                {
+                    printf("\n\nInvalid Password!");
+                    Beep(800, 300);
+                }
+                usrFound = 1;
+            }
+        }
+        if (!usrFound)
+        {
+            printf("\n\nUser is not registered!");
+            Beep(800, 300);
+        }
+        fclose(fp);
+        break;
+    case 3:
+        printf("\t\t\t Bye Bye :)");
+        break;
     }
-    // free_memory(name, email, user_name,password);
     return 0;
 }
